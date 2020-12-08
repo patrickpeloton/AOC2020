0a071447464a528e4e61ffa4c6552add3c2f8e50
patrickpeloton <patrick.trasborg@onepeloton.com>
2020-12-07 22:11:54 -0500

    haskell day7

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -7,11 +7,12 @@ import Day3
 import Day4
 import Day5
 import Day6
+import Day7 
 
 main :: IO ()
 main = do
-  contents <- readFile $ dayInput "day6"
-  print $ day6Part2 contents
+  contents <- readFile $ dayInput "day7"
+  print $ day7Part2 contents
 
 dayInput :: String -> String
 dayInput day = "./inputs/" ++ day
